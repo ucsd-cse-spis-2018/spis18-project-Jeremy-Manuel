64c96061d63535501ce2ed69dfb4379134a54ae2
ThatBob9001 <murrutia@ucsd.edu>
2018-08-30 14:47:00 -0700

    Combined render update and color calculation.

--- harmony.py
+++ harmony.py
@@ -1,4 +1,4 @@
-
+from flask import Flask, url_for, render_template, request 
 from colorsys import rgb_to_hls
 from colorsys import hls_to_rgb
 
@@ -7,7 +7,19 @@ app = Flask(__name__)
 @app.route("/")
 def render_main():
     return render_template('home.html')
-# CODE GOES HERE
+
+@app.route("/colorpick")
+def render_colorpick():
+    return render_template('colorpick.html')
+
+@app.route("/colorresult")
+def render_colorResults():
+    try:
+        color_result = float(request.args['inColor'])
+        # PROCESS COLOR
+        return render_template('colorResult.html', inColor=color_result, compColor=comp_result)
+    except ValueError:
+        return "Sorry: something went wrong."
 
 def convertRGB(red, green , blue):
     difference = 0
@@ -17,7 +29,6 @@ def convertRGB(red, green , blue):
         difference = hue - 180
         hue = difference
     else:
-
         (hue, lightness, saturation) = (hue + 180, lightness, saturation)
 
     print (colorValue)
